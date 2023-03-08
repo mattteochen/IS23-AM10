88be38c2c0da81a29c4d9680029dc23b921cfc71
mattteochen <kaiximatteo.chen@mail.polimi.it>
2023-03-08 14:37:22 +0000

    [Doc] added missing doc

--- is23am10/src/main/java/it/polimi/is23am10/utils/exceptions/NullIndexValueException.java
+++ is23am10/src/main/java/it/polimi/is23am10/utils/exceptions/NullIndexValueException.java
@@ -1,5 +1,13 @@
 package it.polimi.is23am10.utils.exceptions;
 
+/**
+ * Custom exception for null index values.
+ *
+ * @author Alessandro Amandonico (alessandro.amandonico@mail.polimi.it)
+ * @author Francesco Buccoliero (francesco.buccoliero@mail.polimi.it)
+ * @author Kaixi Matteo Chen (kaiximatteo.chen@mail.polimi.it)
+ * @author Lorenzo Cavallero (lorenzo1.cavallero@mail.polimi.it)
+ */
 public class NullIndexValueException extends Exception {
 	public NullIndexValueException() {
 		super("The provided index value is null");
