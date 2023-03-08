c67f091a2be27022342e1bcc3f60e18e89938322
mattteochen <kaiximatteo.chen@mail.polimi.it>
2023-03-08 16:21:56 +0000

    [Factory] added Game class factory

---
+++ is23am10/src/main/java/it/polimi/is23am10/game/exceptions/NullMaxPlayerException.java
@@ -0,0 +1,15 @@
+package it.polimi.is23am10.game.exceptions;
+
+/**
+ * Custom exception to handle null max players value.
+ *
+ * @author Alessandro Amandonico (alessandro.amandonico@mail.polimi.it)
+ * @author Francesco Buccoliero (francesco.buccoliero@mail.polimi.it)
+ * @author Kaixi Matteo Chen (kaiximatteo.chen@mail.polimi.it)
+ * @author Lorenzo Cavallero (lorenzo1.cavallero@mail.polimi.it)
+ */
+public class NullMaxPlayerException extends Exception {
+	public NullMaxPlayerException() {
+		super("Null game id");
+	}
+}
