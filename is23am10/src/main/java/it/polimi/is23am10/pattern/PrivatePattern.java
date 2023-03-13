f1cd072b1e70ce3dbade374988ec54d5527a4f12
locavallero <lorenzo1.cavallero@mail.polimi.it>
2023-03-13 13:08:23 +0000

    [Model] Built PrivatePattern constructor

--- is23am10/src/main/java/it/polimi/is23am10/pattern/PrivatePattern.java
+++ is23am10/src/main/java/it/polimi/is23am10/pattern/PrivatePattern.java
@@ -1,6 +1,9 @@
 package it.polimi.is23am10.pattern;
 
 import it.polimi.is23am10.items.bookshelf.Bookshelf;
+import it.polimi.is23am10.items.bookshelf.exceptions.WrongCharBookshelfStringException;
+import it.polimi.is23am10.items.bookshelf.exceptions.WrongLengthBookshelfStringException;
+import it.polimi.is23am10.items.tile.exceptions.WrongTileTypeException;
 
 /**
  * Private pattern object.
@@ -17,4 +20,22 @@ public class PrivatePattern extends AbstractPattern {
    * 
    */
   private Bookshelf grid;
+  
+  /**
+   * The constructor of the class PrivatePattern.
+   * 
+   */
+  public PrivatePattern(String s){
+    try {
+      this.grid = new Bookshelf(s);
+    } catch (NullPointerException e) {
+      e.printStackTrace();
+    } catch (WrongLengthBookshelfStringException e) {
+      e.printStackTrace();
+    } catch (WrongCharBookshelfStringException e) {
+      e.printStackTrace();
+    } catch (WrongTileTypeException e) {
+      e.printStackTrace();
+    }
+  };
 }
