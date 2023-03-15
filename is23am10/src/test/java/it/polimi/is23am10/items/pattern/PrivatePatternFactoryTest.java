a77dad4be6b13ba661a4e4e4e72d23c48930b558
locavallero <lorenzo1.cavallero@mail.polimi.it>
2023-03-15 07:57:04 +0000

    [Test] Added test for each P. Pattern

--- is23am10/src/test/java/it/polimi/is23am10/items/pattern/PrivatePatternFactoryTest.java
+++ is23am10/src/test/java/it/polimi/is23am10/items/pattern/PrivatePatternFactoryTest.java
@@ -1,5 +1,117 @@
 package it.polimi.is23am10.items.pattern;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import it.polimi.is23am10.items.bookshelf.Bookshelf;
+import it.polimi.is23am10.items.bookshelf.exceptions.WrongCharBookshelfStringException;
+import it.polimi.is23am10.items.bookshelf.exceptions.WrongLengthBookshelfStringException;
+import it.polimi.is23am10.items.tile.exceptions.WrongTileTypeException;
+import it.polimi.is23am10.pattern.PrivatePatternFactory;
+
 public class PrivatePatternFactoryTest {
+
+  @Test
+  public void TEST_PRIVATE_PATTERNS_SHOULD_RETURN_TRUE()
+      throws WrongLengthBookshelfStringException, WrongCharBookshelfStringException,
+      NullPointerException, WrongTileTypeException {
+    Bookshelf testPattern1 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+
+    Bookshelf testPattern2 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+
+    Bookshelf testPattern3 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+          
+    Bookshelf testPattern4 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+
+    Bookshelf testPattern5 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
     
+    Bookshelf testPattern6 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+    Bookshelf testPattern7 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+    Bookshelf testPattern8 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+    Bookshelf testPattern9 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+
+    Bookshelf testPattern10 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+
+    Bookshelf testPattern11 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+
+    Bookshelf testPattern12 = new Bookshelf(
+            "CCBGF" +
+            "FTBFC" +
+            "FCCGF" +
+            "CBGFT" +
+            "PPCBC" +
+            "BCBPC");
+
+    assertTrue(PrivatePatternFactory.checkPattern1.test(testPattern1));
+
+  };
 }
