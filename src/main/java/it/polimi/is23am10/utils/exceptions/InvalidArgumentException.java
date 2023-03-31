ac8b0ab81068caee4d7889e01a7e19e87f7ee836
aleama01 <alessandro.amandonico@mail.polimi.it>
2023-03-31 08:15:17 +0000

    [Model] added invalid argument exception

---
+++ src/main/java/it/polimi/is23am10/utils/exceptions/InvalidArgumentException.java
@@ -0,0 +1,15 @@
+package it.polimi.is23am10.utils.exceptions;
+
+/**
+ * Custom exception for invalid cli argument parsed.
+ *
+ * @author Alessandro Amandonico (alessandro.amandonico@mail.polimi.it)
+ * @author Francesco Buccoliero (francesco.buccoliero@mail.polimi.it)
+ * @author Kaixi Matteo Chen (kaiximatteo.chen@mail.polimi.it)
+ * @author Lorenzo Cavallero (lorenzo1.cavallero@mail.polimi.it)
+ */
+public class InvalidArgumentException extends Exception {
+  public InvalidArgumentException() {
+    super("The provided argument is not valid");
+  }
+}
