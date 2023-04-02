9971b7138d867c37c89a5ec26ccf784659d7d74a
locavallero <lorenzo1.cavallero@mail.polimi.it>
2023-04-02 15:53:41 +0000

    [Minor] Fixes after pr review

--- src/main/java/it/polimi/is23am10/utils/MoveValidator.java
+++ src/main/java/it/polimi/is23am10/utils/MoveValidator.java
@@ -14,7 +14,10 @@ import it.polimi.is23am10.utils.exceptions.TilesInCornerException;
 import it.polimi.is23am10.utils.exceptions.TilesInDiagonalException;
 import it.polimi.is23am10.utils.exceptions.TilesWithoutOneFreeSideException;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * The validator for player's move on the game board and on the player's
@@ -25,7 +28,7 @@ import java.util.Map;
  * @author Kaixi Matteo Chen (kaiximatteo.chen@mail.polimi.it)
  * @author Lorenzo Cavallero (lorenzo1.cavallero@mail.polimi.it)
  */
-public class MoveValidator {
+public final class MoveValidator {
 
   private MoveValidator() {
 
@@ -35,7 +38,7 @@ public class MoveValidator {
    * Input validation for Player's move on board.
    *
    * @param gameBoard           The game board.
-   * @param selectedCoordinates map of coordinates representing the selected
+   * @param selectedCoordinates Map of coordinates representing the selected
    *                            moves.
    * @return True is the move is valid.
    * @throws BoardGridRowIndexOutOfBoundsException
@@ -54,54 +57,50 @@ public class MoveValidator {
       TilesWithoutOneFreeSideException, TilesInCornerException,
       TilesInDiagonalException {
 
-    if (movesAccepted(selectedCoordinates)) {
-      if (hasOneFreeSide(gameBoard, selectedCoordinates)) {
-        if (areTileNotCorner(gameBoard, selectedCoordinates)) {
-          if (tilesNotDiagonal(selectedCoordinates)) {
-            return true;
-          } else {
-            throw new TilesInDiagonalException();
-          }
-        } else {
-          throw new TilesInCornerException();
-        }
-      } else {
-        throw new TilesWithoutOneFreeSideException();
-      }
-    } else {
+    if (!movesAccepted(selectedCoordinates)) {
       throw new MovesNotLessThanThreeException();
     }
+    if (!tilesNotDiagonal(gameBoard, selectedCoordinates)) {
+      throw new TilesInDiagonalException();
+    }
+    return true;
   }
 
   /**
    * Private method to check if the selected tiles are not disposed in diagonal.
    *
-   * @param selectedCoordinates map of coordinates representing the selected
+   * @param selectedCoordinates Map of coordinates representing the selected
    *                            moves.
-   * @return true if the selected tiles are not in diagonal, false otherwise.
+   * @return True if the selected tiles are not in diagonal, false otherwise.
+   * @throws TilesWithoutOneFreeSideException
+   * @throws NullIndexValueException
+   * @throws BoardGridColIndexOutOfBoundsException
+   * @throws BoardGridRowIndexOutOfBoundsException
    */
-  private static boolean tilesNotDiagonal(Map<Coordinates, Coordinates> selectedCoordinates) {
+  private static boolean tilesNotDiagonal(Board gameBoard,
+      Map<Coordinates, Coordinates> selectedCoordinates)
+      throws TilesWithoutOneFreeSideException, BoardGridRowIndexOutOfBoundsException,
+      BoardGridColIndexOutOfBoundsException, NullIndexValueException {
     int[] rows = new int[selectedCoordinates.size()];
     int[] cols = new int[selectedCoordinates.size()];
-
     int i = 0;
     for (Coordinates coord : selectedCoordinates.keySet()) {
+      if (!hasOneFreeSide(gameBoard, coord)) {
+        throw new TilesWithoutOneFreeSideException();
+      }
       rows[i] = coord.getRow();
       cols[i] = coord.getCol();
       i++;
     }
-    int minRow = Arrays.stream(rows).min().getAsInt();
-    int maxRow = Arrays.stream(rows).max().getAsInt();
-    int minCol = Arrays.stream(cols).min().getAsInt();
-    int maxCol = Arrays.stream(cols).max().getAsInt();
+    return (Arrays.stream(rows).allMatch(s -> s == rows[0])
+        || Arrays.stream(cols).allMatch(s -> s == cols[0]));
 
-    return (maxRow - minRow == 0 || maxCol - minCol == 0);
   }
 
   /**
    * Input validation for Player's move on the bookshelf.
    *
-   * @param selectedCoordinates map of coordinates representing the selected
+   * @param selectedCoordinates Map of coordinates representing the selected
    *                            moves.
    * @return True if the move is valid.
    * @throws NotEnoughSlotsException
@@ -110,21 +109,17 @@ public class MoveValidator {
    * @throws BookshelfGridColIndexOutOfBoundsException
    */
   public static boolean isValidMoveOnBookshelf(Bookshelf bookshelf,
-      Map<Coordinates, Coordinates> selectedCoordinates)
+      Collection<Coordinates> selectedCoordinates)
       throws NotEnoughSlotsException, BookshelfGridColIndexOutOfBoundsException,
       BookshelfGridRowIndexOutOfBoundsException, NullIndexValueException {
-    if (hasEnoughFreeSlotsInColumn(bookshelf, selectedCoordinates)) {
-      int prevCol = -1;
-      for (Map.Entry<Coordinates, Coordinates> entry : selectedCoordinates.entrySet()) {
-        Coordinates bsCoord = entry.getValue();
-        int col = bsCoord.getCol();
-        if (prevCol == -1) {
-          prevCol = col;
-        } else if (prevCol != col) {
-          return false;
-        }
-      }
-      return true;
+    int colIndex = selectedCoordinates.iterator().next().getCol();
+    int requestedSlots = selectedCoordinates.size();
+    if (hasEnoughFreeSlotsInColumn(bookshelf, colIndex, requestedSlots)) {
+      return (new HashSet<>(selectedCoordinates
+          .stream()
+          .map(Coordinates::getCol)
+          .collect(Collectors.toSet()))
+          .size() == 1);
     } else {
       throw new NotEnoughSlotsException();
     }
@@ -136,115 +131,68 @@ public class MoveValidator {
    *
    * @param playerBookshelf The player's private bookshelf.
    * @param numberOfTiles   The number of tiles to put in the bookshelf.
-   * @return true if the column has enough free slots, false otherwise
+   * @return True if the column has enough free slots, false otherwise
    * @throws NullIndexValueException
    * @throws BookshelfGridRowIndexOutOfBoundsException
    * @throws BookshelfGridColIndexOutOfBoundsException
    */
   private static boolean hasEnoughFreeSlotsInColumn(Bookshelf bookshelf,
-      Map<Coordinates, Coordinates> selectedCoordinates)
+      int colIndex, int requestedSlots)
       throws BookshelfGridColIndexOutOfBoundsException,
       BookshelfGridRowIndexOutOfBoundsException, NullIndexValueException {
-    int requestedSlots = selectedCoordinates.size();
-    int freeColSlots = 0;
-    int requestedCol = selectedCoordinates.get(selectedCoordinates.keySet().toArray()[0]).getCol();
+    int freeSlots = 0;
     for (int i = 0; i < Bookshelf.BOOKSHELF_ROWS; i++) {
-      Tile t = bookshelf.getBookshelfGridAt(i, requestedCol);
+      Tile t = bookshelf.getBookshelfGridAt(i, colIndex);
       if (t.isEmpty()) {
-        freeColSlots++;
+        freeSlots++;
       }
     }
-    return (freeColSlots >= requestedSlots);
+    return (freeSlots >= requestedSlots);
   }
 
   /**
    * Private method to check if a tile in a selected set has at least one free
    * side.
    *
-   * @param gameBoard     The game board
-   * @param selectedTiles map of coordinates representing the selected moves.
-   * @return true if at least one selected tile has a free side, false otherwise
+   * @param gameBoard The game board
+   * @param coord     Coordinates of the selected tile to be checked.
+   * @return True if the selected tile has one free side, false otherwise.
    * @throws BoardGridRowIndexOutOfBoundsException
    * @throws BoardGridColIndexOutOfBoundsException
    * @throws NullIndexValueException
    */
   private static boolean hasOneFreeSide(Board gameBoard,
-      Map<Coordinates, Coordinates> selectedTiles)
+      Coordinates coord)
       throws BoardGridRowIndexOutOfBoundsException,
       BoardGridColIndexOutOfBoundsException,
       NullIndexValueException {
-    for (Coordinates coord : selectedTiles.keySet()) {
-      int row = coord.getRow();
-      int col = coord.getCol();
-      int freeSides = 0;
-      if (gameBoard.getTileAt(row, col + 1).isEmpty()) {
-        freeSides++;
-      }
-      if (gameBoard.getTileAt(row, col - 1).isEmpty()) {
-        freeSides++;
-      }
-      if (gameBoard.getTileAt(row + 1, col).isEmpty()) {
-        freeSides++;
-      }
-      if (gameBoard.getTileAt(row - 1, col).isEmpty()) {
-        freeSides++;
-      }
-      if (freeSides == 0) {
-        return false;
-      }
+    int row = coord.getRow();
+    int col = coord.getCol();
+    int freeSides = 0;
+    if (gameBoard.getTileAt(row, col + 1).isEmpty()) {
+      freeSides++;
     }
-    return true;
+    if (gameBoard.getTileAt(row, col - 1).isEmpty()) {
+      freeSides++;
+    }
+    if (gameBoard.getTileAt(row + 1, col).isEmpty()) {
+      freeSides++;
+    }
+    if (gameBoard.getTileAt(row - 1, col).isEmpty()) {
+      freeSides++;
+    }
+    return freeSides > 0;
   }
 
   /**
    * Determines whether a set of selected coordinates is valid for a move.
    *
-   * @param selectedCoordinates map of coordinates representing the selected
+   * @param selectedCoordinates Map of coordinates representing the selected
    *                            moves.
-   * @return true if the number of selected coordinates is less than or equal to
+   * @return True if the number of selected coordinates is less than or equal to
    *         3, false otherwise
    */
   private static boolean movesAccepted(Map<Coordinates, Coordinates> selectedCoordinates) {
-    return selectedCoordinates.size() <= 3;
-  }
-
-  /**
-   * Private method to check if no selected tiles are in a corner.
-   *
-   * @param gameBoard           The game board
-   * @param selectedCoordinates map of coordinates representing the selected
-   *                            moves.
-   * @return true if no selected tiles are in corner.
-   * @throws BoardGridRowIndexOutOfBoundsException
-   * @throws BoardGridColIndexOutOfBoundsException
-   * @throws NullIndexValueException
-   */
-  private static boolean areTileNotCorner(Board gameBoard,
-      Map<Coordinates, Coordinates> selectedCoordinates)
-      throws BoardGridRowIndexOutOfBoundsException,
-      BoardGridColIndexOutOfBoundsException, NullIndexValueException {
-
-    for (Coordinates coord : selectedCoordinates.keySet()) {
-      int row = coord.getRow();
-      int col = coord.getCol();
-
-      int numAdjacentTiles = 0;
-      if (gameBoard.getTileAt(row, col + 1).isEmpty()) {
-        numAdjacentTiles++;
-      }
-      if (gameBoard.getTileAt(row, col - 1).isEmpty()) {
-        numAdjacentTiles++;
-      }
-      if (gameBoard.getTileAt(row + 1, col).isEmpty()) {
-        numAdjacentTiles++;
-      }
-      if (gameBoard.getTileAt(row - 1, col).isEmpty()) {
-        numAdjacentTiles++;
-      }
-      if (numAdjacentTiles >= 2) {
-        return false;
-      }
-    }
-    return true;
+    return selectedCoordinates.size() > 0 && selectedCoordinates.size() <= 3;
   }
 }
--- src/test/java/it/polimi/is23am10/utils/MoveValidatorTest.java
+++ src/test/java/it/polimi/is23am10/utils/MoveValidatorTest.java
@@ -1,7 +1,6 @@
 package it.polimi.is23am10.utils;
 
 import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import it.polimi.is23am10.items.board.Board;
@@ -9,13 +8,13 @@ import it.polimi.is23am10.items.board.exceptions.BoardGridColIndexOutOfBoundsExc
 import it.polimi.is23am10.items.board.exceptions.BoardGridRowIndexOutOfBoundsException;
 import it.polimi.is23am10.items.board.exceptions.InvalidNumOfPlayersException;
 import it.polimi.is23am10.items.board.exceptions.NullNumOfPlayersException;
+import it.polimi.is23am10.items.board.exceptions.WrongCharGameBoardStringException;
+import it.polimi.is23am10.items.board.exceptions.WrongLengthGameBoardStringException;
 import it.polimi.is23am10.items.bookshelf.Bookshelf;
 import it.polimi.is23am10.items.bookshelf.exceptions.BookshelfGridColIndexOutOfBoundsException;
 import it.polimi.is23am10.items.bookshelf.exceptions.BookshelfGridRowIndexOutOfBoundsException;
 import it.polimi.is23am10.items.bookshelf.exceptions.WrongCharBookshelfStringException;
 import it.polimi.is23am10.items.bookshelf.exceptions.WrongLengthBookshelfStringException;
-import it.polimi.is23am10.items.tile.Tile;
-import it.polimi.is23am10.items.tile.Tile.TileType;
 import it.polimi.is23am10.utils.exceptions.MovesNotLessThanThreeException;
 import it.polimi.is23am10.utils.exceptions.NotEnoughSlotsException;
 import it.polimi.is23am10.utils.exceptions.NullIndexValueException;
@@ -24,8 +23,8 @@ import it.polimi.is23am10.utils.exceptions.TilesInDiagonalException;
 import it.polimi.is23am10.utils.exceptions.TilesWithoutOneFreeSideException;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test class to check the valid and illegal moves.
@@ -40,69 +39,30 @@ public class MoveValidatorTest {
       throws InvalidNumOfPlayersException,
       NullNumOfPlayersException, NullPointerException,
       WrongLengthBookshelfStringException,
-      WrongCharBookshelfStringException {
-    gameBoard = new Board(4);
+      WrongCharBookshelfStringException,
+      WrongLengthGameBoardStringException, WrongCharGameBoardStringException {
     bookshelf = new Bookshelf(
-              "XXXXC"
+        "XXXXC"
             + "XXXCC"
             + "XXXPP"
             + "XPPPP"
             + "XCCCC"
             + "TTTTT");
 
-    Tile[][] grid = {
-        { new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) },
-        { new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) },
-        { new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.CAT), new Tile(TileType.CAT),
-            new Tile(TileType.PLANT), new Tile(TileType.TROPHY),
-            new Tile(TileType.CAT), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) },
-        { new Tile(TileType.EMPTY), new Tile(TileType.GAME),
-            new Tile(TileType.BOOK), new Tile(TileType.FRAME),
-            new Tile(TileType.BOOK), new Tile(TileType.PLANT),
-            new Tile(TileType.PLANT), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) },
-        { new Tile(TileType.CAT), new Tile(TileType.GAME),
-            new Tile(TileType.BOOK), new Tile(TileType.FRAME),
-            new Tile(TileType.TROPHY), new Tile(TileType.PLANT),
-            new Tile(TileType.BOOK), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) },
-        { new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.PLANT),
-            new Tile(TileType.CAT), new Tile(TileType.PLANT),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) },
-        { new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.FRAME), new Tile(TileType.PLANT),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) },
-        { new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.PLANT),
-            new Tile(TileType.PLANT), new Tile(TileType.CAT),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) },
-        { new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY), new Tile(TileType.EMPTY),
-            new Tile(TileType.EMPTY) }
-    };
-
-    gameBoard.setBoardGrid(grid);
+    gameBoard = new Board(4,
+        "XXXXXXXXX"
+            + "XXXXXXXXX"
+            + "XXCCPTCXX"
+            + "XXBFBPCXX"
+            + "CGTFGTFXX"
+            + "XXXCTFXXX"
+            + "XXXXPFXXX"
+            + "XXXPCFXXX"
+            + "XXXXXXXXX");
   }
 
   @Test
-  public void testIsValidMoveOnBoard_validMove()
+  public void IsValidMoveOnBoard_should_return_true() 
       throws BoardGridRowIndexOutOfBoundsException,
       BoardGridColIndexOutOfBoundsException, NullIndexValueException,
       MovesNotLessThanThreeException, TilesWithoutOneFreeSideException,
@@ -119,12 +79,11 @@ public class MoveValidatorTest {
     selectedCoordinates.put(boardCoord2, bsCoord2);
     selectedCoordinates.put(boardCoord3, bsCoord3);
 
-
     assertTrue(MoveValidator.isValidMoveOnBoard(gameBoard, selectedCoordinates));
   }
 
   @Test
-  public void testIsValidMoveOnBoard_invalidMove_MovesNotLessThanThreeException()
+  public void IsValidMoveOnBoard_should_throw_MovesNotLessThanThreeException()
       throws BoardGridRowIndexOutOfBoundsException,
       BoardGridColIndexOutOfBoundsException, NullIndexValueException,
       MovesNotLessThanThreeException, TilesWithoutOneFreeSideException,
@@ -149,7 +108,7 @@ public class MoveValidatorTest {
   }
 
   @Test
-  public void testIsValidMoveOnBoard_invalidMove_TilesWithoutOneFreeSideException() {
+  public void IsValidMoveOnBoard_should_throw_TilesWithoutOneFreeSideException() {
     Map<Coordinates, Coordinates> selectedCoordinates = new HashMap<>();
 
     final Coordinates boardCoord1 = new Coordinates(2, 3);
@@ -167,25 +126,7 @@ public class MoveValidatorTest {
   }
 
   @Test
-  public void testIsValidMoveOnBoard_invalidMove_TilesInCornerException() {
-    Map<Coordinates, Coordinates> selectedCoordinates = new HashMap<>();
-
-    final Coordinates boardCoord1 = new Coordinates(2, 2);
-    final Coordinates boardCoord2 = new Coordinates(2, 3);
-    final Coordinates boardCoord3 = new Coordinates(2, 4);
-    final Coordinates bsCoord1 = new Coordinates(4, 0);
-    final Coordinates bsCoord2 = new Coordinates(3, 0);
-    final Coordinates bsCoord3 = new Coordinates(2, 0);
-    selectedCoordinates.put(boardCoord1, bsCoord1);
-    selectedCoordinates.put(boardCoord2, bsCoord2);
-    selectedCoordinates.put(boardCoord3, bsCoord3);
-
-    assertThrows(TilesInCornerException.class,
-        () -> MoveValidator.isValidMoveOnBoard(gameBoard, selectedCoordinates));
-  }
-
-  @Test
-  public void testIsValidMoveOnBoard_invalidMove_TilesInDiagonalException() {
+  public void IsValidMoveOnBoard_should_throw_TilesInDiagonalException() {
     Map<Coordinates, Coordinates> selectedCoordinates = new HashMap<>();
 
     final Coordinates boardCoord1 = new Coordinates(7, 4);
@@ -200,7 +141,7 @@ public class MoveValidatorTest {
   }
 
   @Test
-  public void testIsValidMoveOnBookshelf_validMove()
+  public void IsValidMoveOnBookshelf_should_return_true()
       throws BookshelfGridColIndexOutOfBoundsException,
       BookshelfGridRowIndexOutOfBoundsException, NotEnoughSlotsException,
       NullIndexValueException {
@@ -216,11 +157,11 @@ public class MoveValidatorTest {
     selectedCoordinates.put(boardCoord2, bsCoord2);
     selectedCoordinates.put(boardCoord3, bsCoord3);
 
-    assertTrue(MoveValidator.isValidMoveOnBookshelf(bookshelf, selectedCoordinates));
+    assertTrue(MoveValidator.isValidMoveOnBookshelf(bookshelf, selectedCoordinates.values()));
   }
 
   @Test
-  public void testIsValidMoveOnBookshelf_invalidMove_NotEnoughSlotsException() {
+  public void IsValidMoveOnBookshelf_should_throw_NotEnoughSlotsException() {
     Map<Coordinates, Coordinates> selectedCoordinates = new HashMap<>();
 
     final Coordinates boardCoord1 = new Coordinates(2, 3);
@@ -234,6 +175,6 @@ public class MoveValidatorTest {
     selectedCoordinates.put(boardCoord3, bsCoord3);
 
     assertThrows(NotEnoughSlotsException.class,
-        () -> MoveValidator.isValidMoveOnBookshelf(bookshelf, selectedCoordinates));
+        () -> MoveValidator.isValidMoveOnBookshelf(bookshelf, selectedCoordinates.values()));
   }
 }
