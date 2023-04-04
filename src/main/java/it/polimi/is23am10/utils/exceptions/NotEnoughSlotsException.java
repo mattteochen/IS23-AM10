5de3a2b049aaa0a291cacc25cb1918e1f05ae88d
locavallero <lorenzo1.cavallero@mail.polimi.it>
2023-04-04 09:04:36 +0000

    [feature] added exception for n. slots in bs

--- src/main/java/it/polimi/is23am10/utils/exceptions/NotEnoughSlotsException.java
+++ src/main/java/it/polimi/is23am10/utils/exceptions/NotEnoughSlotsException.java
@@ -9,7 +9,7 @@ package it.polimi.is23am10.utils.exceptions;
  * @author Lorenzo Cavallero (lorenzo1.cavallero@mail.polimi.it)
  */
 
-public class NotEnoughSlotsException extends Exception{
+public class NotEnoughSlotsException extends Exception {
   public NotEnoughSlotsException() {
     super("The number on selected tiles exceed the number of free slots in the bookshelf column");
   }
