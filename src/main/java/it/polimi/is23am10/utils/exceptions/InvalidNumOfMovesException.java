282ee95385f7231b89b1469b1416e741650f92ec
locavallero <lorenzo1.cavallero@mail.polimi.it>
2023-04-04 09:03:47 +0000

    [Feature] added exce. for invalid num of moves

---
+++ src/main/java/it/polimi/is23am10/utils/exceptions/InvalidNumOfMovesException.java
@@ -0,0 +1,15 @@
+package it.polimi.is23am10.utils.exceptions;
+
+/**
+ * Custom exception for more than tree moves.
+ *
+ * @author Alessandro Amandonico (alessandro.amandonico@mail.polimi.it)
+ * @author Francesco Buccoliero (francesco.buccoliero@mail.polimi.it)
+ * @author Kaixi Matteo Chen (kaiximatteo.chen@mail.polimi.it)
+ * @author Lorenzo Cavallero (lorenzo1.cavallero@mail.polimi.it)
+ */
+public class InvalidNumOfMovesException extends Exception {
+  public InvalidNumOfMovesException() {
+    super("The provided moves are more than tree");
+  }
+}
