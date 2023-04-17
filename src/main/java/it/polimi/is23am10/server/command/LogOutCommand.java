274c2cab7b379f8492680c742a7432ca25f0d9fa
locavallero <lorenzo1.cavallero@mail.polimi.it>
2023-04-17 16:53:07 +0000

    [Feature] Added LogOutCommand

---
+++ src/main/java/it/polimi/is23am10/server/command/LogOutCommand.java
@@ -0,0 +1,67 @@
+package it.polimi.is23am10.server.command;
+
+/**
+ * The log out command class definition.
+ *
+ * @author Alessandro Amandonico (alessandro.amandonico@mail.polimi.it)
+ * @author Francesco Buccoliero (francesco.buccoliero@mail.polimi.it)
+ * @author Kaixi Matteo Chen (kaiximatteo.chen@mail.polimi.it)
+ * @author Lorenzo Cavallero (lorenzo1.cavallero@mail.polimi.it)
+ */
+public class LogOutCommand extends AbstractCommand {
+  /**
+   * The player name who has logged out.
+   * 
+   */
+  private String playerName;
+
+  /**
+   * An utility to be used during deserialization processes.
+   * 
+   */
+  @SuppressWarnings("unused")
+  private final String className = this.getClass().getName();
+
+  /**
+   * Constructor.
+   *
+   * @param playerName The player name who has logged out.
+   */
+  public LogOutCommand(String playerName) {
+    super(Opcode.LOGOUT);
+    this.playerName = playerName;
+  }
+
+  /**
+   * Player name getter.
+   *
+   * @return The player name.
+   */
+  public String getPlayerName(){
+    return playerName;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof LogOutCommand)) {
+      return false;
+    }
+
+    LogOutCommand casted = (LogOutCommand) obj;
+
+    return (opcode == casted.getOpcode() && playerName.equals(casted.getPlayerName()));
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   */
+  @Override
+  public int hashCode() {
+    return playerName.hashCode();
+  }
+}
